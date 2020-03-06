f598558b733ea13e9ebcfdeb107dda1bfe3d41cb
hw <2195792949@qq.com>
2020-03-06 21:44:23 +0800

    java

---
+++ 20_02_26/Test.java
@@ -0,0 +1,29 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+        int c=3;
+		if(a<b) {
+			if(a<c) {
+			System.out.printfln(a);
+			}
+		System.out.printfln(c);
+		}
+	System.out.printfln(a);
+	}
+		
+	public static void main1(String[] args) {
+		int num1=5;
+		int num2=6;
+		int x=0;
+		x=num1;
+		num1=num2;
+		num2=x;
+		System.out.println(num1);
+		System.out.println(num2);
+		
+	
+	}
+}
