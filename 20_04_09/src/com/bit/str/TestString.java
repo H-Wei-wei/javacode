2acf0b3b634f08acec7c5b14f29a33b064852131
hw <2195792949@qq.com>
2020-04-12 00:00:59 +0800

    1

--- 20_04_09/src/com/bit/str/TestString.java
+++ 20_04_09/src/com/bit/str/TestString.java
@@ -5,20 +5,44 @@ import java.util.Arrays;
 public class TestString {
 
     public static void main(String[] args) {
+        String str = "\n\t      abc      \n\t";
+        System.out.println("["+str+"]");
+        System.out.println("["+str.trim()+"]");
+
+    }
+
+    public static void main5(String[] args) {
+        String str = "hello world";
+        //前闭后开  截取
+        System.out.println(str.substring(6,9));
+        //表示截取下标6以后的字符
+        System.out.println(str.substring(6));
+
+
+    }
+
+    public static void main4(String[] args) {
         /*String str1 = "aaa,bbb,,ccc";
 
         //字符串分割 split
         String[] ret = str1.split(",");
         System.out.println(Arrays.toString(ret));*/
 
-        String str = "199.168.1.1";
+        //String str = "199.168.1.1";
         // . 在正则表达式中有特别含义，这里的切分并不是按照 "." 来切分的
         // 如果要按照 . 的文本格式切割，就得转义
         // 正则中通过 \. 来查找文本
         // java中字符串又不能用直接写 \，需要 \\ 来表示 \
         // | * + - 都有类似的情况
-        String[] ret = str.split("\\.");
-        System.out.println(Arrays.toString(ret));
+        /*String[] ret = str.split("\\.");
+        System.out.println(Arrays.toString(ret));*/
+
+        String str = "name=张三&age= 18";
+        String[] result = str.split("&");
+        for (String tmp:result) {
+            String[] tmp2 = tmp.split("=");
+            System.out.println(tmp2[0]+":"+tmp2[1]);
+        }
 
 
     }
---
+++ 20_04_10/src/TestDemo.java
@@ -0,0 +1,98 @@
+import java.util.Random;
+
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main6(String[] args) {
+        for (int i = 1; i < 999999; i++) {
+            int count = 0;
+            int num = i;
+            //判断个数
+            while(num != 0) {
+                count++;
+                num = num / 10;
+            }
+
+            int sum = 0;
+            num = i;
+            while( num!= 0) {
+                sum += Math.pow(num%10,count);
+                num = num/10;
+            }
+            //比较
+            if(i == sum) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    public static void main5(String[] args) {
+        Random random = new Random();
+        int rand = random.nextInt(100);
+        System.out.println(rand);
+    }
+
+    public static void main4(String[] args) {
+        int i = 0;
+        int j = 0;
+        for (i = 1; i <= 100; i++) {
+            for (j = 2; j < i/2; j++) {
+                if(i % j == 0){
+                    break;
+                }
+            }
+            if(j >= i/2){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main3(String[] args) {
+        int i = 0;
+        int j = 0;
+        for (i = 1; i <= 100; i++) {
+            for (j = 2; j < i; j++) {
+                if(i % j == 0){
+                    break;
+                }
+            }
+            if(j >= i){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main2(String[] args) {
+        for (int year = 1000; year <= 2000; year++) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.print(year+" ");
+            }
+        }
+    }
+
+    public static void main1(String[] args) {
+        int count = 0;
+        for (int i = 1; i <= 100; i++) {
+            /*int tmp = i;
+            while(tmp != 0) {
+               if (tmp % 10 == 9) {
+                   count++;
+               }
+               tmp = tmp / 10;
+            }*/
+            //个位
+            if(i % 10 == 9) {
+                count++;
+            }
+            //十位
+            if(i / 10 == 9){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+}
