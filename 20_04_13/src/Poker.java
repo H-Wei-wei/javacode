d3bd5519d41b7baf4c1614935838e226f12cc21d
hw <2195792949@qq.com>
2020-04-14 23:37:05 +0800

    List--Poker

---
+++ 20_04_13/src/Poker.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Code {
+    public String suit;
+    public String
+}
+public class Poker {
+    List<String> Poker = new ArrayList<>();
+}
---
+++ 20_04_13/src/TestList.java
@@ -0,0 +1,26 @@
+
+class Test extends Object {
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
+
+public class TestList {
+    public static void main(String[] args) {
+        // 匿名内部类
+        // 内部类是指定义在某个类内或是某个方法内的类
+        // 匿名是指没有创建类的名字，只知道这个类是个 Object 类的子类
+        // { } 括号里是这个内部类的实现代码
+       /* Object o = new Object() {
+                @Override
+                public boolean equals(Object obj) {
+                    return super.equals(obj);
+                }
+
+            };
+        o.equals(10);*/
+
+       Object o = new Test();
+    }
+}
