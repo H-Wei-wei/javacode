709b978fb6a80ef34f2500bdd040b9c3874dd9c9
hw <2195792949@qq.com>
2020-05-20 00:45:40 +0800

    TreeInterview

--- 20_05_19/src/BuildTree.java
+++ 20_05_19/src/BuildTree.java
@@ -27,15 +27,36 @@ public class BuildTree {
         }
     }
 
-    private static void inOrder(TreeNode root) {
+    // 记录字符串字符下标
+    public static int index = 0;
 
+    // 中序遍历
+    private static void inOrder(TreeNode root) {
+         if (root == null) {
+             return;
+         }
+         inOrder(root.left);
+        System.out.print(root.val+" ");
+         inOrder(root.right);
     }
 
     private static TreeNode buildTree(String line) {
+        // 可能会有多组数据，每个字符串都得从 0 开始遍历
+        index = 0;
         return buildTreeHelper(line);
     }
     // 辅助递归的方法
     private static TreeNode buildTreeHelper(String line) {
-        
+        char ch = line.charAt(index);
+        if (ch == '#') {
+            return null;
+        }
+        // 创建节点
+        TreeNode node = new TreeNode(ch);
+        index++;
+        node.left = buildTreeHelper(line);
+        index++;
+        node.right = buildTreeHelper(line);
+        return node;
     }
 }
